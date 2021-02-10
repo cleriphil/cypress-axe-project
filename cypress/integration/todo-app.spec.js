c4847d7494483353b43e9d28a3854dd76cd4e6eb
cleriphil <cleri.phil@gmail.com>
2021-02-10 13:27:59 -0800

    refactor to implement beforeeach

---
+++ cypress/integration/todo-app.spec.js
@@ -0,0 +1,20 @@
+/// <reference types="cypress"/>
+ 
+describe('Todo application', () => {
+  beforeEach(() => {
+    cy.visit('http://todomvc.com/examples/react');
+    cy.injectAxe();
+  });
+
+  it('should log any accessibility failures', () => {
+    cy.checkA11y();
+  });
+
+  it('should exclude specific elements on the page', () => {
+    cy.checkA11y({ exclude: ['.learn'] });
+  });
+
+  it('should only test specific element on the page', () => {
+    cy.checkA11y('.learn');
+  });
+});
